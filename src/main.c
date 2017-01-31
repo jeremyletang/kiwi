f6c2d5a7e68216f78c654009d9c981dd2232723b
jeremyletang <letang.jeremy@gmail.com>
2017-01-31 00:14:42 +0000

    add find function

--- src/main.c
+++ src/main.c
@@ -578,6 +578,50 @@ int write_rev(FILE *fh, FILE *fc, int ac, char *av[]) {
   return _write_rev_decl(fh, ac, av) | _write_rev_def(fc, ac, av);
 }
 
+int _write_find_decl(FILE *f, int ac, char *av[]) {
+  int err = 0;
+  err |= fprintf(f, "#define find(lst, f) _Generic((lst)");
+  // write the macro first
+  for (int i = 1; i != ac; i += 1) {
+    err |= fprintf(f, ", \\\n  %s_list*: %s_list_find", av[i], av[i]);
+  }
+  err |= fprintf(f, ")(lst, f)\n\n");
+  // then func declarations
+  for (int i = 1; i != ac; i += 1) {
+    err |= fprintf(f, "const %s *%s_list_find(%s_list *, bool (*)(const %s*));\n",
+                   av[i], av[i], av[i], av[i]);
+  }
+  err |= fprintf(f, "\n");
+
+  return err;
+}
+
+int _write_find_def(FILE *f, int ac, char *av[]) {
+    const char *fmt =
+      "const %s *%s_list_find(%s_list *lst, bool (*f)(const %s*)) {\n"
+      "  if (lst) {\n"
+      "    %s_value *_first = lst->first;\n"
+      "    for (; _first; _first = _first->next) {\n"
+      "      if (f(&_first->value)) {\n"
+      "        return &_first->value;\n"
+      "      }\n"
+      "    }\n"
+      "  }\n"
+      "  return 0;\n"
+      "}\n\n";
+
+  int err = 0;
+  for (int i = 1; i != ac; i +=1) {
+    err |= fprintf(f, fmt, av[i], av[i], av[i], av[i], av[i]);
+  }
+
+  return err;
+}
+
+int write_find(FILE *fh, FILE *fc, int ac, char *av[]) {
+  return _write_find_decl(fh, ac, av) | _write_find_def(fc, ac, av);
+}
+
 int write_includes(FILE *fh, FILE *fc) {
   const char *fmtc =
     "#include \"kiwi.h\"\n"
@@ -615,6 +659,7 @@ int do_stuff(int ac, char *av[]) {
   write_any(fh, fc, ac, av);
   write_all(fh, fc, ac, av);
   write_rev(fh, fc, ac, av);
+  write_find(fh, fc, ac, av);
   // endif include guards
   fprintf(fh, "#endif // KIWI_H\n");
 
